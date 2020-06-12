2375332f5cd9fdd8fe78216989a047e497bbffb0
zzVIPz <viperexe@mail.ru>
2020-06-11 21:14:51 -0400

    feat: add basic project configuration

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = (env, options) => {
+  const isProduction = options.mode === 'production';
+
+  const config = {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'none' : 'source-map',
+    watch: !isProduction,
+    entry: {
+      index: ['./src/index.js', './src/sass/index.scss'],
+      main: ['./src/pages/main/main.js', './src/sass/main.scss'],
+    },
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: './src/js/[name].js',
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+        {
+          test: /\.scss$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(png|svg|jpe?g|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[path][name].[ext]',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.html$/,
+          loader: 'html-loader',
+        },
+      ],
+    },
+    devServer: {
+      contentBase: './dist',
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        chunks: ['index'],
+      }),
+      new HtmlWebpackPlugin({
+        filename: 'pages/main.html',
+        template: 'src/pages/main.html',
+        chunks: ['main'],
+      }),
+      new CopyPlugin([
+        { from: 'src/assets/images', to: 'src/assets/images' },
+        { from: 'src/assets/media', to: 'src/assets/media' },
+        { from: 'src/assets/svg', to: 'src/assets/svg' },
+      ]),
+      new MiniCssExtractPlugin({
+        filename: './src/css/[name].css',
+      }),
+    ],
+  };
+
+  return config;
+};
