65a42d7a9200a308bd5a014329621a6bafd0a9cc
NordOst88 <NordOst88@users.noreply.github.com>
2020-06-18 03:25:23 +0400

    feat: add layout and apply base styles

--- src/js/games/english-puzzle/models/englishPuzzleModel.js
+++ src/js/games/english-puzzle/models/englishPuzzleModel.js
@@ -4,4 +4,13 @@ export default class EnglishPuzzleModel {
   constructor() {
     this.data = DATA_FOR_GAMES;
   }
+
+  getSentences() {
+    const sentences = [];
+    this.data.forEach((el) => {
+      const currentSentence = el.textExample.replace(/<[^<>]+>/g, '');
+      sentences.push(currentSentence);
+    });
+    return sentences;
+  }
 }
--- src/js/games/english-puzzle/views/englishPuzzleView.js
+++ src/js/games/english-puzzle/views/englishPuzzleView.js
@@ -3,9 +3,43 @@ import EnglishPuzzleModel from '../models/englishPuzzleModel';
 export default class EnglishPuzzleView {
   constructor() {
     this.template = `
-      <div>
-      <h1>ПРЮВЭТ!!</h1>
+      <div class="english-puzzle">
+        <div class="controls">
+          <div class="controls__left-side">
+            <label>
+              Level
+              <select name="levelSelect">
+                <option value="value1">1</option> 
+                <option value="value2" selected>2</option>
+                <option value="value3">3</option>
+              </select>
+            </label>
+            <label>
+              Page
+              <select name="pageSelect">
+                <option value="value1">1</option> 
+                <option value="value2">2</option>
+                <option value="value3" selected>3</option>
+              </select>
+            </label>
+          </div>
+          <div class="controls__right-side">
+            <div class="controls__btn"></div>
+            <div class="controls__btn"></div>
+            <div class="controls__btn"></div>
+            <div class="controls__btn"></div>
+          </div>
+        </div>
+        <div class="tips">
+          <div class="controls__btn"></div>
+          <p class="text-translate">Some text<p>
+        </div>
         <div class="mockData"></div>
+        <div class="playground"></div>
+        <div class="play-buttons">
+          <button class="english-puzzle__btn" name="skip">I don't know</button>
+          <button class="english-puzzle__btn" name="check">Check</button>
+        </div>
       </div>
       `;
     this.englishPuzzleModel = new EnglishPuzzleModel();
@@ -13,9 +47,11 @@ export default class EnglishPuzzleView {
 
   render() {
     document.querySelector('.main').innerHTML = this.template;
-    this.englishPuzzleModel.data.forEach((el) => {
+    const sentences = this.englishPuzzleModel.getSentences();
+    sentences.forEach((el) => {
       const elem = document.createElement('div');
-      elem.textContent = el.word;
+      elem.classList.add('sentences');
+      elem.textContent = el;
       document.querySelector('.mockData').append(elem);
     });
   }
