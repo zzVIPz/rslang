eeb787377e6398f58801a6d294d77b983c8d4f0a
NordOst88 <NordOst88@users.noreply.github.com>
2020-06-20 18:19:48 +0400

    refactor: styles and apply csscomb

--- src/js/games/english-puzzle/views/template.js
+++ src/js/games/english-puzzle/views/template.js
@@ -2,17 +2,17 @@ const template = `
       <div class="english-puzzle">
         <div class="ep-controls">
           <div class="ep-controls__left-side">
-            <label>
+            <label class="ep-label">
               Level
-              <select name="levelSelect">
+              <select class="ep-select" name="levelSelect">
                 <option value="value1">1</option> 
                 <option value="value2" selected>2</option>
                 <option value="value3">3</option>
               </select>
             </label>
-            <label>
+            <label class="ep-label">
               Page
-              <select name="pageSelect">
+              <select class="ep-select" name="pageSelect">
                 <option value="value1">1</option> 
                 <option value="value2">2</option>
                 <option value="value3" selected>3</option>
@@ -20,22 +20,22 @@ const template = `
             </label>
           </div>
           <div class="ep-controls__right-side">
-            <div class="ep-controls__btn autospeech"></div>
-            <div class="ep-controls__btn tips"></div>
-            <div class="ep-controls__btn speechsentence"></div>
-            <div class="ep-controls__btn bg-image"></div>
+            <div class="ep__button tips__button tips__button_autospeech"></div>
+            <div class="ep__button tips__button tips__button_translate"></div>
+            <div class="ep__button tips__button tips__button_speech"></div>
+            <div class="ep__button tips__button tips__button_background"></div>
           </div>
         </div>
         <div class="ep-tips">
-          <div class="ep-controls__btn autospeech-grey"></div>
+          <div class="ep__button tips__button tips__button_autospeech-grey"></div>
           <p class="ep-tips__text-translate">Some text from current sentence<p>
         </div>
         <div class="ep-board"></div>
         <div id="playField" class="ep-play__field ep-board__line drag-container"></div>
         <div class="ep-play__buttons">
-          <button id="skipBtn" class="ep-play__btn" name="skip">I don't know</button>
-          <button id="checkBtn" class="ep-play__btn ep-hidden" name="check">Check</button>
-          <button id="continueBtn" class="ep-play__btn ep-hidden" name="continue">Continue</button>
+          <button id="skipBtn" class="ep__button" name="skip">I don't know</button>
+          <button id="checkBtn" class="ep__button ep-hidden" name="check">Check</button>
+          <button id="continueBtn" class="ep__button ep-hidden" name="continue">Continue</button>
         </div>
       </div>
       `;
