6b08d7a510442c0002af7fa776f78fa694040f84
NordOst88 <NordOst88@users.noreply.github.com>
2020-07-04 16:24:14 +0400

    feat: add styles for basic structure

--- src/js/components/dictionary/templateControls.js
+++ src/js/components/dictionary/templateControls.js
@@ -1,5 +1,5 @@
 const templateControls = `
-  <div class="dictionary>
+  <div class="dictionary">
     <div class="dictionary__controls">
       <div class="controlsBtn">EasyWords</div>
       <div class="controlsBtn">DifficultWords</div>
