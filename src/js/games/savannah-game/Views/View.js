d2e5558dcdd4d326b8e8bddf935a15dc402f191b
zzVIPz <viperexe@mail.ru>
2020-07-04 14:37:51 -0400

    feat: add responsive styles

--- src/js/games/savannah-game/Views/View.js
+++ src/js/games/savannah-game/Views/View.js
@@ -59,7 +59,9 @@ class SavannahView {
         this.mainContainer.removeChild(this.savannahContainer);
       }
     } else {
-      setTimeout(() => { this.checkSavannahWindow(); }, DELAY / 2);
+      setTimeout(() => {
+        this.checkSavannahWindow();
+      }, DELAY / 2);
     }
   }
 
@@ -130,12 +132,13 @@ class SavannahView {
       this.chosenLevel = this.level;
       this.chosenRound = this.round;
       this.addPreloader();
-      setTimeout(() => { this.preloaderCountDown(); }, DELAY);
-      this.model.fetchWords(this.currentUser, this.chosenLevel, this.chosenRound)
-        .then((data) => {
-          this.model.getWordsAndTranslation(data);
-          this.model.getWordIdsAndAudio(data);
-        });
+      setTimeout(() => {
+        this.preloaderCountDown();
+      }, DELAY);
+      this.model.fetchWords(this.currentUser, this.chosenLevel, this.chosenRound).then((data) => {
+        this.model.getWordsAndTranslation(data);
+        this.model.getWordIdsAndAudio(data);
+      });
     });
   }
 
@@ -154,8 +157,12 @@ class SavannahView {
 
   preloaderCountDown() {
     if (this.model.isPreloading) {
-      const activeMenu = document.querySelector('.burger-menu').classList.contains('burger-menu--active');
-      const visibleModal = document.querySelector('.app__modal').classList.contains('app__modal_visible');
+      const activeMenu = document
+        .querySelector('.burger-menu')
+        .classList.contains('burger-menu--active');
+      const visibleModal = document
+        .querySelector('.app__modal')
+        .classList.contains('app__modal_visible');
       const settingsVisible = document.querySelector('.settings__overlay');
 
       if (!activeMenu && !visibleModal && !settingsVisible) {
@@ -163,12 +170,16 @@ class SavannahView {
 
         if (this.countNumber > 0) {
           document.querySelector('.countdown').innerHTML = this.countNumber;
-          setTimeout(() => { this.preloaderCountDown(); }, DELAY);
+          setTimeout(() => {
+            this.preloaderCountDown();
+          }, DELAY);
         } else {
           this.gameMode();
         }
       } else {
-        setTimeout(() => { this.preloaderCountDown(); }, DELAY);
+        setTimeout(() => {
+          this.preloaderCountDown();
+        }, DELAY);
       }
     }
   }
@@ -187,7 +198,7 @@ class SavannahView {
     const wrongAnswer = false;
 
     if (translationEl) {
-      const answer = (translationEl.textContent).replace(this.model.removeDigitsRegExp, '');
+      const answer = translationEl.textContent.replace(this.model.removeDigitsRegExp, '');
       const result = this.model.isRightTranslation(answer);
 
       this.model.getCurrentAudioUrl();
@@ -206,7 +217,9 @@ class SavannahView {
         this.wrongTranslationActions(translationEl, wrongAnswer, rightAnswer);
       }
 
-      setTimeout(() => { this.nextWord(); }, DELAY);
+      setTimeout(() => {
+        this.nextWord();
+      }, DELAY);
     }
   }
 
@@ -218,9 +231,14 @@ class SavannahView {
     this.flyingWord.classList.add('flying-word_hide');
     this.bangOnRightAnswer();
 
-    setTimeout(() => { this.removeHighlight(translationEl, rightAnswer); }, DELAY);
-    this.gameStatistics.appendCorrectAnswer(this.randomEngWord,
-      this.model.correctAnswer, this.model.currentWordAudio);
+    setTimeout(() => {
+      this.removeHighlight(translationEl, rightAnswer);
+    }, DELAY);
+    this.gameStatistics.appendCorrectAnswer(
+      this.randomEngWord,
+      this.model.correctAnswer,
+      this.model.currentWordAudio,
+    );
   }
 
   wrongTranslationActions(translationEl, wrongAnswer, rightAnswer) {
@@ -231,12 +249,19 @@ class SavannahView {
     clearInterval(this.id);
     this.flyingWord.classList.add('flying-word_hide');
 
-    setTimeout(() => { this.removeHighlight(translationEl, wrongAnswer); }, DELAY);
-    setTimeout(() => { this.removeHighlight(this.correctHTMLEl, rightAnswer); }, DELAY);
+    setTimeout(() => {
+      this.removeHighlight(translationEl, wrongAnswer);
+    }, DELAY);
+    setTimeout(() => {
+      this.removeHighlight(this.correctHTMLEl, rightAnswer);
+    }, DELAY);
     this.removeLives(this.model.wrongAnswerCounter);
 
-    this.gameStatistics.appendWrongAnswer(this.randomEngWord,
-      this.model.correctAnswer, this.model.currentWordAudio);
+    this.gameStatistics.appendWrongAnswer(
+      this.randomEngWord,
+      this.model.correctAnswer,
+      this.model.currentWordAudio,
+    );
   }
 
   clickTranslation() {
@@ -357,10 +382,12 @@ class SavannahView {
   frame() {
     const rightAnswer = true;
     const settingsVisible = document.querySelector('.settings__overlay');
-    const menuActive = document.querySelector('.burger-menu').classList.contains('burger-menu--active');
+    const menuActive = document
+      .querySelector('.burger-menu')
+      .classList.contains('burger-menu--active');
 
     if (!menuActive && !settingsVisible) {
-      if (this.pos >= (this.marginTop * MARGIN_DEADLINE_COEFICIENT) && this.model.isGameOn) {
+      if (this.pos >= this.marginTop * MARGIN_DEADLINE_COEFICIENT && this.model.isGameOn) {
         clearInterval(this.id);
         if (this.model.audioOn) {
           this.errorSound.play();
@@ -372,12 +399,19 @@ class SavannahView {
 
         this.removeLives();
         this.highlightAnswer(this.correctHTMLEl, rightAnswer);
-        this.gameStatistics.appendWrongAnswer(this.randomEngWord,
-          this.model.correctAnswer, this.model.currentWordAudio);
+        this.gameStatistics.appendWrongAnswer(
+          this.randomEngWord,
+          this.model.correctAnswer,
+          this.model.currentWordAudio,
+        );
         this.flyingWord.classList.add('flying-word_hide');
         this.flyingWord.style.top = '0';
-        setTimeout(() => { this.removeHighlight(this.correctHTMLEl, rightAnswer); }, DELAY);
-        setTimeout(() => { this.nextWord(); }, DELAY);
+        setTimeout(() => {
+          this.removeHighlight(this.correctHTMLEl, rightAnswer);
+        }, DELAY);
+        setTimeout(() => {
+          this.nextWord();
+        }, DELAY);
       } else {
         this.pos += this.marginTop / BASE_MARGIN;
         this.flyingWord.style.top = `${this.pos}px`;
@@ -407,7 +441,11 @@ class SavannahView {
     const spanArr = [];
 
     fourAnswersWordsArr.forEach((item, index) => {
-      spanArr.push(`<span class="translation translation-${index + 1}"><span class="keyboard-num">${index + 1}</span>${item}</span>`);
+      spanArr.push(
+        `<span class="translation translation-${index + 1}"><span class="keyboard-num">${
+          index + 1
+        }</span>${item}</span>`,
+      );
     });
     this.translationBox.innerHTML = spanArr.join('');
     this.appContent.appendChild(this.translationBox);
@@ -441,7 +479,9 @@ class SavannahView {
       this.model.audioOn = false;
     }
 
-    setTimeout(() => { this.setMusicOnOff(); }, DELAY);
+    setTimeout(() => {
+      this.setMusicOnOff();
+    }, DELAY);
   }
 
   moveBackground() {
@@ -460,7 +500,7 @@ class SavannahView {
     } else {
       element.classList.add('wrong-answer');
     }
-  }
+  };
 
   removeHighlight = (element, isRight) => {
     if (isRight) {
@@ -468,7 +508,7 @@ class SavannahView {
     } else {
       element.classList.remove('wrong-answer');
     }
-  }
+  };
 
   findCorrectAnswerHTMLel() {
     this.answerId = this.model.findCorrectAnswerId();
@@ -531,7 +571,9 @@ class SavannahView {
     const marginTop = document.documentElement.clientHeight * MARGIN_PERCENTAGE;
     if (this.marginTop !== marginTop && document.querySelector('.app__content__translation-box')) {
       this.marginTop = marginTop;
-      document.querySelector('.app__content__translation-box').style.marginTop = `${this.marginTop}px`;
+      document.querySelector(
+        '.app__content__translation-box',
+      ).style.marginTop = `${this.marginTop}px`;
     }
   }
 }
