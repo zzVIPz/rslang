4efb35d70a111b173348426c79482fe0428ab10d
DmitryIP <dmitryip@tut.by>
2020-07-05 13:02:30 +0300

    feat: add short statistics template

--- src/js/utils/getShortStatisticsTemplate.js
+++ src/js/utils/getShortStatisticsTemplate.js
@@ -4,13 +4,27 @@ export default function getShortStatisticsTemplate({
   newWordsAmount,
   correctAnswersSeries,
 }) {
-  // todo: here need implement temlate
-  const template = `
-  <p>${cardsTotal}</p>
-  <p>${percentageCorrectAnswers}</p>
-  <p>${newWordsAmount}</p>
-  <p>${correctAnswersSeries}</p>
-  `;
+  const template = `<div class="short-stat-container">
+  <div class="short-stat__logo"></div>
+  <div class="short-stat__title">Серия завершена</div>
+  <div class="short-stat__info short-stat__info_finished">
+      <div class="short-stat__text">Карточек завершено:</div>
+      <div class="short-stat__data">${cardsTotal}</div>
+  </div>
+  <div class="short-stat__info short-stat__info_correct">
+      <div class="short-stat__text">Правильные ответы:</div>
+      <div class="short-stat__data">${percentageCorrectAnswers}</div>
+  </div>
+  <div class="short-stat__info short-stat__info_new">
+      <div class="short-stat__text">Новые слова:</div>
+      <div class="short-stat__data">${newWordsAmount}</div>
+  </div>
+  <div class="short-stat__info short-stat__info_series">
+      <div class="short-stat__text">Самая длинная серия правильных ответов:</div>
+      <div class="short-stat__data">${correctAnswersSeries}</div>
+  </div>
+  <button class="short-stat__button">Продолжить</button>
+</div>`;
 
   return template;
 }
