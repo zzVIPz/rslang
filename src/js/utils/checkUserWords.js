87aa66c29a34f3bac5dc562847acfc54c1064ffd
vitali30 <60606326+vitali30@users.noreply.github.com>
2020-07-07 08:54:13 +0300

    add check user words file

---
+++ src/js/utils/checkUserWords.js
@@ -0,0 +1,13 @@
+import MainModel from '../models/mainModel';
+
+export default async function checkUserWords(array) {
+  const mainModel = new MainModel();
+  const result = await mainModel.getAllUsersWords();
+  for (const oneId of array) {
+    for (let i = 0; i < result.length; i += 1) {
+      if (oneId === result[i].wordId && result[i].difficulty === 'easy') {
+        mainModel.updateUserWord(oneId, 'repeat');
+      }
+    }
+  }
+}
