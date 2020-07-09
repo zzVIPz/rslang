27e508032ccb33ccf877ea8aa1eaf5d11020003d
NordOst88 <NordOst88@users.noreply.github.com>
2020-07-09 17:26:31 +0400

    feat: add statistics methods for counting games starts and learning words

--- src/js/components/dailyStatistics/dailyStatisticsConstants.js
+++ src/js/components/dailyStatistics/dailyStatisticsConstants.js
@@ -1,13 +1,13 @@
 const STAT_CONST = {
-  learnedWords: 80,
+  learnedWords: 50,
   optional: {
     games: {
       speak: 0, puzzle: 1, call: 2, savanna: 3, sprint: 4, newGame: 5,
     },
     progress: {
       '01/01/2020': 10,
-      '02/01/2020': 20,
-      '05/01/2020': 50,
+      '01/02/2020': 20,
+      '01/05/2020': 50,
     },
   },
 };
--- src/js/components/dailyStatistics/dailyStatisticsController.js
+++ src/js/components/dailyStatistics/dailyStatisticsController.js
@@ -7,22 +7,46 @@ export default class DailyStatisticsController {
   }
 
   async init() {
-    // await this.mainModel.setUserStatistic(this.statObj);
     // console.log(this.mainModel.userId, this.mainModel.token);
-    await this.mainModel.getUserStatistic();
     this.getDate();
+    await this.getData();
+    await this.setDateInProgress();
   }
 
   getDate() {
-    this.currentDate = new Date();
-    let day = this.currentDate.getDate();
+    const currentDate = new Date();
+    let day = currentDate.getDate();
     if (day < 10) day = `0${day}`;
 
-    let month = this.currentDate.getMonth() + 1;
+    let month = currentDate.getMonth() + 1;
     if (month < 10) month = `0${month}`;
 
-    const year = this.currentDate.getFullYear();
-    const dateString = `${day}/${month}/${year}`;
-    console.log(dateString);
+    const year = currentDate.getFullYear();
+    this.date = `${month}/${day}/${year}`;
+  }
+
+  async getData() {
+    const data = await this.mainModel.getUserStatistic();
+    delete data.id;
+    this.statData = data;
+  }
+
+  async setData() {
+    await this.mainModel.setUserStatistic(this.statData);
+  }
+
+  async learnedWordsUpdate(count) {
+    this.statData.learnedWords += count;
+    await this.setDateInProgress();
+  }
+
+  async gameStartsStat(game) {
+    this.statData.optional.games[game] += 1;
+    await this.setData();
+  }
+
+  async setDateInProgress() {
+    this.statData.optional.progress[this.date] = this.statData.learnedWords;
+    await this.setData();
   }
 }
