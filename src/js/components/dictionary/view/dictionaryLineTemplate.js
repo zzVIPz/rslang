61d5e0e011c6b337d7c9d6408de42965accfb480
NordOst88 <NordOst88@users.noreply.github.com>
2020-07-10 02:51:32 +0400

    feat: add new data for words view

--- src/js/components/dictionary/view/dictionaryLineTemplate.js
+++ src/js/components/dictionary/view/dictionaryLineTemplate.js
@@ -16,6 +16,10 @@ const dictionaryLineTemplate = (data, audioSrc, user, state) => `
   <div class="dict__word-remove ${(user.dictionaryControl && (state === CONSTANTS.DICT_STATES.LEARNING)) ? '' : 'hidden'}" data-id="${data._id}"></div>
   <div class="dict__word-restore ${(user.dictionaryControl && (state === CONSTANTS.DICT_STATES.REMOVED || state === CONSTANTS.DICT_STATES.DIFFICULT)) ? '' : 'hidden'}" data-id="${data._id}"></div>
 </div>
+<div class="dict__optional">
+  <div class="dict__repeatCounter">Count of repeats: <span>${data.userWord.optional.repeatCounter}</span></div>
+  <div class="dict__lastTimeRepeat">Last time repeat: <span>${new Date(data.userWord.optional.lastTimeRepeat).toString().slice(0, 15)}</span></div>
+</div>
 `;
 
 export default dictionaryLineTemplate;
--- src/js/components/dictionary/view/dictionaryView.js
+++ src/js/components/dictionary/view/dictionaryView.js
@@ -24,6 +24,7 @@ export default class DictionaryController {
   }
 
   renderLines(data, user, state) {
+    console.log(data);
     this.domElements.wordsData.innerHTML = '';
     if (!data.length) {
       const noWords = document.createElement('div');
