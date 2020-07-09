e46498a1b0ebc9b24330bbff9adf7eefe51570a0
vitali30 <60606326+vitali30@users.noreply.github.com>
2020-07-09 20:58:07 +0300

    fix PR comment

--- src/js/utils/checkUserWords.js
+++ src/js/utils/checkUserWords.js
@@ -4,7 +4,7 @@ import { WORDS_STATUS } from '../constants/constMainView';
 export default async function checkUserWords(array) {
   const mainModel = new MainModel();
   const result = await mainModel.getAllUsersWords();
-  for (let i = 0; i < array; i += 1) {
+  for (let i = 0; i < array.length; i += 1) {
     for (let j = 0; j < result.length; j += 1) {
       if (array[i] === result[j].wordId) {
         switch (result[j].difficulty) {
