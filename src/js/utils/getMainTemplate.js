b236c398390c549732c07c447633209e437c1cf0
zzVIPz <viperexe@mail.ru>
2020-07-11 21:06:27 -0400

    feat: add style for user info

--- src/js/utils/getMainTemplate.js
+++ src/js/utils/getMainTemplate.js
@@ -1,37 +1,39 @@
 export default function getMainTemplate(user, data, achievements) {
   return `
     <div class="user-info container">
-      <h3 class="user-info__title">${data.title} ${user.username}</h3>
+      <h3 class="user-info__title">${data.title}
+        <span class="user-info__username">${user.username}</span>
+      </h3>
       <p class="user-info__subtitle">${data.subtitle}</p>
       <div class="user-info__container wrapper">
         <p class="user-info__text">${data.totalCards}</p>
-        <p class="user-info__text"> ${user.cardsTotal}</p>
+        <p class="user-info__value"> ${user.cardsTotal}</p>
       </div>
       <div class="user-info__container wrapper">
         <p class="user-info__text">${data.newWords}</p>
-        <p class="user-info__text"> ${user.cardsNew}</p>
+        <p class="user-info__value"> ${user.cardsNew}</p>
       </div>
       <div class="user-info__container wrapper">
         <p class="user-info__text">${data.studyMode}</p>
-        <p class="user-info__text">${user.studyMode}</p>
+        <p class="user-info__value">${user.studyMode}</p>
       </div>
       <p class="user-info__subtitle">${data.achievements}</p>
       <div class="user-info__container wrapper">
         <p class="user-info__text">${data.passedWords}</p>
-        <p class="user-info__text">${achievements.allUserWords} out of ${data.amountCards}</p>
+        <p class="user-info__value">${achievements.allUserWords} out of ${data.amountCards}</p>
       </div>
       <div class="user-info__container wrapper">
         <p class="user-info__text">${data.learnedWords}</p>
-        <p class="user-info__text">${achievements.learnedWords}</p>
+        <p class="user-info__value">${achievements.learnedWords}</p>
       </div>
       <div class="user-info__container wrapper">
         <p class="user-info__text">${data.progress}</p>
-        <p class="user-info__text">
+        <p class="user-info__value">
           ${+((achievements.learnedWords * 100) / data.amountCards).toFixed(2)}%
         </p>
       </div>
       <div class="user-info__buttons wrapper">
-      <button class="user-info__button btn-start">${data.btnStart}</button>
-    </div>
+        <button class="user-info__button btn-start">${data.btnStart}</button>
+      </div>
     </div>`;
 }
